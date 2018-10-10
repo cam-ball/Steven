4472761156f487a736923c66f33fa0ca06bcf9e3
Jerrgree <jeremyrgreenburg@gmail.com>
2018-10-10 18:31:39 -0500

    Reuse old variable

--- lib/Steven/admin.rb
+++ lib/Steven/admin.rb
@@ -3,18 +3,18 @@ module Steven
   module Admin
     extend Discordrb::Commands::CommandContainer
     command :addaction do |event, action, *user_info|
-      @user = user_info.join(" ")
+      user_info = user_info.join(" ")
       if event.author.id == CONFIG.owner_id
-        unless @user && action
+        unless user_info && action
           event.respond "Please provide a valid user name or ID and action"
         end
         action = action.to_sym
 
-        if @user.to_i.positive?
-          user_id = @user.to_i
+        if user_info.to_i.positive?
+          user_id = user_info.to_i
           username = lookup_by_user_id(event.server.id, user_id)
         else
-          username = @user
+          username = user_info
           user_id = lookup_by_username(event.server.id, username)
 
           user_id = user_id.first unless user_id.size > 1
