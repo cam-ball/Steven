10f5f66bbfd672e9da27ddbe669fb1d72274466f
laurenball <lauren.ball@annkissam.com>
2020-04-18 22:37:54 -0400

    Add Coddler and Hazer specs

---
+++ spec/steven/coddler_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Steven::Coddler do
+  let(:event) { message_event(content: 'any ole thing') }
+  let(:affirmations) do
+    YAML.load_file("#{Dir.pwd}/data/affirmations.yml")
+  end
+
+  before { allow(Steven::Responder).to receive(:call) }
+
+  it do
+    dispatch(Discordrb::Events::MessageEvent, event)
+
+    expect(Steven::Responder).to have_received(:call).once
+      .with(event, :affirm, affirmations)
+  end
+end
