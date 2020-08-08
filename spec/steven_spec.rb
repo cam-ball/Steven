a72cab6bde9541ce8e348a1db48a95f89d27c98f
laurenball <lauren.ball@annkissam.com>
2020-08-08 02:48:09 -0400

    Resolve failing spec

--- spec/steven_spec.rb
+++ spec/steven_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 RSpec.describe Steven do
   it "has a version number" do
-    expect(Steven::VERSION).to eq('0.1.0')
+    expect(Steven::VERSION).to eq('0.3.0')
   end
 
   it { expect(Steven::BOT).to be_a Discordrb::Commands::CommandBot }
